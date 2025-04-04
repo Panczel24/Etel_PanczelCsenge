2a3cf44de6f14cd31758bdfe5e41da7e44df2e95
Petrik Diak <diak@petrik.hu>
2025-04-04 12:30:14 +0200

    Étel osztály

---
+++ Étel/Étel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Étel
+{
+	internal abstract class Étel
+	{
+		double caloriesPerDkg;
+		double weight;
+		string name;
+
+		public bool IsItDairyFree { get; set; }
+		public double CaloriesPerDkg { get => caloriesPerDkg; set => caloriesPerDkg = value; }
+		public double Weight { get => weight; set => weight = value; }
+		public string Name { get => name; set => name = value; }
+
+		//egy bool típusú publikusan írható és olvasható IsItDairyFree tulajdonságot - ha nagy betű és tulajdonság, akkor bizto kell get és set
+
+		protected Étel(double caloriesPerDkg, double weight, string name)
+		{
+			this.caloriesPerDkg = caloriesPerDkg;
+			this.weight = weight;
+			this.name = name;
+			this.IsItDairyFree = false;
+		}
+
+		public abstract double CountCalories();//láthatósaá, jellemző, típus
+
+
+	}
+}
