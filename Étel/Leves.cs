b96908000a1b73d45bb5e134b0afd581341bb4e1
Petrik Diak <diak@petrik.hu>
2025-04-04 12:43:28 +0200

    Leves, kenyér, hús

---
+++ Étel/Leves.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Étel
+{
+	internal class Leves : Étel
+	{
+		public Leves(double caloriesPerDkg, double weight, string name, bool isItDairyFree) : base(caloriesPerDkg, weight, name)
+		{
+			IsItDairyFree = isItDairyFree;
+		}
+
+		public override double CountCalories()
+		{
+			return (CaloriesPerDkg * Weight) / 100;
+		}
+	}
+}
